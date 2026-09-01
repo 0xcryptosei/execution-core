c2048fc3eebf9f4bb7691814f7a7e3e250985536
0xcryptosei <seiroman1111.@gmail.com>
2026-09-01 11:47:41 +0300

    refactor: narrow public package exports to core API surface

--- src/execution_core/__init__.py
+++ src/execution_core/__init__.py
@@ -1,51 +1,16 @@
-version = "0.1.0"
-
-from execution_core.clock import Clock, FakeClock, SystemClock
-from execution_core.engine import Engine, EngineContext, KillSwitch, OrderStore, Strategy
-from execution_core.events import Event
-from execution_core.paper_broker import FillMode, PaperBroker
-from execution_core.position import apply_fill, apply_fill_to_account, empty_position
+from execution_core.clock import FakeClock
+from execution_core.engine import Engine, KillSwitch
+from execution_core.paper_broker import PaperBroker
 from execution_core.risk import check
-from execution_core.types import (
-    Account,
-    Fill,
-    Intent,
-    Order,
-    OrderPlan,
-    OrderStatus,
-    OrderType,
-    Position,
-    RiskDecision,
-    RiskLimits,
-    Side,
-)
+from execution_core.types import Intent, OrderPlan, RiskLimits
 
 __all__ = [
-    "Account",
-    "apply_fill",
-    "apply_fill_to_account",
-    "check",
-    "Clock",
     "Engine",
-    "EngineContext",
-    "empty_position",
-    "Event",
     "FakeClock",
-    "Fill",
-    "FillMode",
     "Intent",
     "KillSwitch",
-    "Order",
     "OrderPlan",
-    "OrderStatus",
-    "OrderType",
-    "OrderStore",
     "PaperBroker",
-    "Position",
-    "RiskDecision",
     "RiskLimits",
-    "Side",
-    "Strategy",
-    "SystemClock",
-    "version",
+    "check",
 ]
